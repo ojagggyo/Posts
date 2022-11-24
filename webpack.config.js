7625dae1e3f58a75d2a5b5d8e73413ad520711e0
ojagggyo <ojagggyo@gmail.com>
2022-11-24 23:16:05 +0900

    m

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         // 出力するファイル名
         filename: 'bundle.js',
         // 出力先のパス（絶対パスを指定する必要がある）
-        path: path.join(__dirname, 'public')
+        //path: path.join(__dirname, 'public')
+        path: path.join(__dirname, 'public/blog')
     },
 };
