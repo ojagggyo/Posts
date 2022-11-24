6f94b88b31bd853714b87a4b16d4d958280d6ee3
ojagggyo <ojagggyo@gmail.com>
2022-11-24 22:37:05 +0900

    m

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ const Koa = require('koa');
 const app = new Koa();
 const serve = require('koa-static');
 app.use(serve('./public'));
-//app.use('/blog', serve('./public'));
-
-app.listen(3000,"0.0.0.0");
-
+app.listen(3000, "0.0.0.0");
 console.log('listening on port 3000');
