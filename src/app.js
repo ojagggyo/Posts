67848c885c496348af1b317bc9b0c7e5f6fd8bd3
ojagggyo <ojagggyo@gmail.com>
2022-11-26 17:44:01 +0900

    m

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ function donokuraimae(date){
 
 	64,495,283
 
-	if(sa >= 2360591558*2){return Math.floor(sa / 86400000)+' moons ago';}//27.32166155*3600*24
+	if(sa >= 2360591558*2){return Math.floor(sa / 2360591558)+' moons ago';}//27.32166155*3600*24
 	if(sa >= 86400000*2){return Math.floor(sa / 86400000)+' days ago';}
 	if(sa >= 3600000*2){return Math.floor(sa / 3600000)+' hours ago';}
 	if(sa >= 60000*2){return Math.floor(sa / 60000)+' minutes ago';}
